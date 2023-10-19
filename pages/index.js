ff9eb41005cb9e648a4eb914ba696780532fe9ed
francomacedo96 <francomacedo@outlook.com>
2023-10-19 10:56:47 -0300

    links

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,9 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      
 
       <h1>Hola Mundo en Next</h1>
+
+      <Link href="/nosotros">Nosotros</Link>
+
     </>
   )
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Nosotros() {
+    return (
+
+
+        <div>nosotros
+
+            <Link href="/">Ir a Inicio</Link>
+
+
+        </div>
+
+
+    )
+}
