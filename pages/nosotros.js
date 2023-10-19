2502b2b9223ece53a6a2902c9deaa420ee4a2873
francomacedo96 <francomacedo@outlook.com>
2023-10-19 15:31:01 -0300

    us

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -13,7 +13,7 @@ export default function Nosotros() {
 
             <main className='contenedor'>
                 <h2 className='heading'>Nosotros</h2>
-                
+
                 <div className={styles.contenido}>
                     <Image
                         src="/img/nosotros.jpg"
@@ -28,11 +28,7 @@ export default function Nosotros() {
                             Pellentesque feugiat erat quis tellus pretium suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec varius tellus eros, at semper purus tristique quis. Nulla vitae molestie dui. Duis vitae ullamcorper metus. Donec cursus porta mollis. Phasellus aliquam ut ligula non rutrum. Quisque vel nisl quis ipsum eleifend varius a ut justo. Etiam nibh tellus, consequat ut interdum vitae, hendrerit non mi. Etiam dapibus lectus ut ligula gravida, nec tempor libero feugiat. Vestibulum imperdiet mauris ac erat finibus, vitae fringilla sem luctus.
                         </p>
 
-                        <p>
-                            Mauris tempor mi pulvinar, ullamcorper metus id, porta libero. Donec ut orci aliquam, sagittis lectus in, interdum quam. Phasellus scelerisque venenatis erat, sit amet maximus mi tincidunt at. Sed vel tristique magna, id aliquet lectus. Donec feugiat, justo ac ultrices iaculis, elit magna ullamcorper augue, non euismod ligula dolor eu nulla. Integer velit quam, fermentum id erat cursus, varius consequat risus. Sed urna tortor, consectetur sit amet maximus vitae, dapibus feugiat enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque malesuada tellus pretium risus feugiat, quis condimentum lectus elementum. Morbi sed sollicitudin eros. Phasellus at nibh mollis, commodo ante vitae, blandit nisl. Curabitur et accumsan quam. Vivamus a mauris a purus ullamcorper luctus in non justo. Morbi sed nulla elementum, posuere ante a, vulputate magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer velit mi, maximus vel tortor venenatis, rutrum tempor nibh.
-
-
-                        </p>
+                        
                     </div>
 
                 </div>
