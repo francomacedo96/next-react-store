6dba6a4d8a900d98ae807401602a7e2343661700
francomacedo96 <francomacedo@outlook.com>
2023-10-20 15:11:46 -0300

    url

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,24 +1,66 @@
 import { useRouter } from "next/router"
+import Image from "next/image"
+import styles from "../../styles/guitarras.module.css"
+import Layout from "@/components/layout"
 
 export default function Producto({guitarra}) {
     const router = useRouter()
-    console.log(guitarra)
+    const {nombre, descripcion, imagen, precio} = guitarra[0].attributes
 
   return (
-    <div>
-        
+
+    <Layout
+      title={`Guitarra ${nombre} `}
+    >
+    <div className={styles.guitarra}>
+      <Image src={imagen.data.attributes.formats.medium.url} width={600} height={400} alt={`Imagen guitarra ${nombre}`} />
+        <div className={styles.contenido}>
+          <h3> {nombre} </h3>
+          <p className={styles.descripcion}> {descripcion} </p>
+          <p className={styles.precio}> $ {precio} </p>
+        </div>
     </div>
+    </Layout>
   )
 }
 
 export async function getServerSideProps({query: {url}}) {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+  const {data: guitarra} = await respuesta.json()
+  console.log(guitarra)
+
+  return {
+      props: {
+        guitarra
+      }
+  }
+}
+
+
+/*
+export async function getServerPaths() {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras`)
+  const {data} = await respuesta.json()
+
+  const paths = data.map(guitarra => ({
+    params: {
+      url: guitarra.attributes.url
+    }
+  }))
+
+  return {
+      paths,
+      fallback: false
+  }
+}
+
+export async function getStaticProps({params: {url}}) {
     const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
     const {data: guitarra} = await respuesta.json()
-    console.log(guitarra)
 
     return {
         props: {
           guitarra
         }
     }
-}
+}*/
